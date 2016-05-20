be7e5cb7908205ca8c97af706da35aa4961a8408
samuelyusunwang <samuelyusunwang@gmail.com>
2016-05-19 21:25:10 -0500

    ts

--- TS Analysis and Its Applications/Appendix_R.R
+++ TS Analysis and Its Applications/Appendix_R.R
@@ -1,1 +1,54 @@
 setwd('F:\\GitHub\\RStudy\\TS Analysis and Its Applications')
+
+load("tsa3.rda")
+
+ls()  # to get a listing of your objects, and
+tsa3.version  # to check this version
+
+2+2 # addition
+
+5*5 + 2 # multiplication and addition
+
+5/5 - 3 # division and subtraction
+
+log(exp(pi)) # log, exponential, pi
+
+sin(pi/2) # sinusoids
+
+exp(1)^(-2) # power
+
+sqrt(8) # square root
+
+1:5 # sequences
+
+seq(1, 10, by=2) # sequences
+
+rep(2, 3) # repeat 2 three times
+
+x <- 1 + 2 # put 1 + 2 in object x
+
+x = 1 + 2 # same as above with fewer keystrokes
+
+(y = 9*3) # put 9 times 3 in y and view the result
+
+(z = rnorm(5,0,1)) # put 5 standard normals into z and print z
+
+ls()  # list all objects
+
+ls(pattern = "my")  # list every object that contains "my"
+
+rm(dummy) # remove object "dummy"
+
+help.start() # html help and documentation (use it)
+
+help(exp) # specific help (?exp is the same)
+
+getwd() # get working directory
+
+setwd("/TimeSeries/") # change working directory to TimeSeries
+
+q() # end the session (keep reading)
+
+
+
+
